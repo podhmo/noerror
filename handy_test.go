7167b4e387e1cf77316bd88b8f2cc2f15bb61de6
podhmo <ababjam61+github@gmail.com>
2019-08-02 00:24:15 +0900

    tests: merge test files

--- handy_test.go
+++ handy_test.go
@@ -6,6 +6,33 @@ import (
 	"github.com/podhmo/handy"
 )
 
+func TestEqual(t *testing.T) {
+	add := func(x, y int) int {
+		return x + y
+	}
+
+	handy.Require(t, handy.Equal(add(10, 20)).Except(30))
+	handy.Assert(t, handy.Equal(add(10, 20)).Except(30))
+
+	handy.Require(t, handy.NotEqual(add(10, 20)).Except(31))
+	handy.Assert(t, handy.NotEqual(add(10, 20)).Except(31))
+}
+
+func TestDeepEqual(t *testing.T) {
+	type Person struct {
+		Name string
+		Age  int
+	}
+
+	p := Person{Name: "foo", Age: 20}
+	p2 := Person{Name: "foo", Age: 20}
+	handy.Require(t, handy.DeepEqual(p).Except(p))
+	handy.Require(t, handy.DeepEqual(&p).Except(&p))
+	handy.Assert(t, handy.DeepEqual(p).Except(p2))
+
+	handy.Assert(t, handy.DeepNotEqual(p).Except(&p))
+}
+
 func TestJSONEqual(t *testing.T) {
 	type Person struct {
 		Name string
